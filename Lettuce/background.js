da25c1ef3102adb79d12127c58a8cae8bc4c6168
tanjo <tanjo@thebitcellar.com>
2014-10-27 20:53:49 +0900

    Initial Commit

---
+++ Lettuce/background.js
@@ -0,0 +1,16 @@
+function openPopup(url) {
+  chrome.tabs.create({ url : url });
+}
+
+function tweet(text, url, selection) {
+  var tweet_url = "https://twitter.com/intent/tweet?text=" + text + " " + url + " " + selection;
+  openPopup(tweet_url);
+}
+
+chrome.browserAction.onClicked.addListener(function(activeTab) {
+  chrome.tabs.executeScript({code: "window.getSelection().toString();"}, function(selection) {
+    chrome.tabs.getSelected(null, function(tab) {
+      tweet(tab.title, tab.url, selection);
+    });
+  });
+});
---
+++ README.md
@@ -0,0 +1,10 @@
+Lettuce
+=======
+
+Tweet Button | Twitter Developers 
+https://dev.twitter.com/web/tweet-button
+
+```
+https://twitter.com/share?url=#{URL}&text=#{title}
+```
+
